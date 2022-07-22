1d2874571969e74c37b403d85dee7d721127ae56
oriascrius <qoq77416416@gmail.com>
2022-07-22 08:46:36 +0800

    20220722
    jQuery 作業 更新

---
+++ jQuery_01-practice/javascript/all.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    // 導航 a 連結 點擊
+    $('.nav-list a').click(function (event) {
+        // 阻止默認動作
+        event.preventDefault();
+
+        // 點擊時 跑出選單
+        $(this).parent().find('ul').slideToggle();
+
+        // 點擊時 其他收起
+        $(this).parent().siblings().find('ul').slideUp();
+
+
+    });
+
+
+    // lightbox 燈箱效果
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+
+
+    // Swiper - 輪播
+    var mySwiper = new Swiper('.swiper', {
+        direction: 'horizontal', // 垂直切換選項
+        loop: true, // 循環模式選擇
+        loopedSlides: 3, // 無限循環
+        speed: 2000, // 翻頁速度
+
+        // 分頁器
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+
+        // 自動撥放
+        autoplay: {
+            delay: 2000, //停留幾秒後下一頁
+            disableOnInteraction: true // 滑鼠點擊之後仍自動播放
+        },
+
+        // 前進後退按紐
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+
+        // 滾動條
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
+
+
+    // 回到 top 按鈕
+    $(".top-btn").click(function (event) {
+        event.preventDefault();
+        $("html,body").animate(
+            {
+                scrollTop: 0
+            },
+            700
+        );
+    });
+
+
+
+});
